82291f750a7b57f9724026287ac7443bdbbeb082
aki <lala.akira@gmail.com>
2023-06-15 10:35:28 +0900

    First commit

---
+++ lib/chrome_testing_downloader.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "chrome_testing_downloader/version"
+
+module ChromeTestingDownloader
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/chrome_testing_downloader/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ChromeTestingDownloader
+  VERSION = "0.1.0"
+end
