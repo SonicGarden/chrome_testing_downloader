bc634002aa87026316af7b47bf915085fee3b547
aki <lala.akira@gmail.com>
2023-06-16 13:40:11 +0900

    feat: Add rake tasks

--- lib/chrome_testing_downloader.rb
+++ lib/chrome_testing_downloader.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
-require_relative 'chrome_testing_downloader/downloader'
+require 'rails'
+require 'open-uri'
+require_relative 'chrome_testing_downloader/railtie'
 require_relative 'chrome_testing_downloader/version'
 
 module ChromeTestingDownloader
+  JSON_URL = 'https://googlechromelabs.github.io/chrome-for-testing/last-known-good-versions.json'
+
   def chrome_path
-    Downloader.new(required_version, cache_directory).chrome_path
+    output = `npx -y @puppeteer/browsers install chrome@#{required_version} --path #{cache_directory}`
+    output.split(' ', 2).last.chomp
   end
 
   def version_file_path
@@ -20,5 +25,10 @@ module ChromeTestingDownloader
     version_file_path.read.chomp
   end
 
-  module_function :chrome_path, :required_version, :version_file_path, :cache_directory
+  def latest_version(channel = 'Stable')
+    json = JSON.parse(OpenURI.open_uri(JSON_URL).read)
+    json.dig('channels', channel, 'version')
+  end
+
+  module_function :chrome_path, :required_version, :version_file_path, :cache_directory, :latest_version
 end
---
+++ lib/tasks/chrome_testing_downloader.rake
@@ -0,0 +1,19 @@
+namespace :chrome do
+  desc 'Download Chrome For Testing'
+  task download: :environment do
+    ChromeTestingDownloader.chrome_path
+  end
+
+  desc 'Check Chrome For Testing version'
+  task check_version: :environment do
+    if ChromeTestingDownloader.latest_version != ChromeTestingDownloader.required_version
+      puts "Chrome For Testing version is old. Please run `rake chrome_testing_downloader:update_version`"
+      exit 1
+    end
+  end
+
+  desc 'Update Chrome For Testing version'
+  task update_version: :environment do
+    File.write(ChromeTestingDownloader.version_file_path, ChromeTestingDownloader.latest_version)
+  end
+end
