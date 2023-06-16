c4cb096d92d54803b82b58c63e41a6b976d6ef0d
aki <lala.akira@gmail.com>
2023-06-16 13:50:36 +0900

    bump

--- spec/chrome_testing_downloader_spec.rb
+++ spec/chrome_testing_downloader_spec.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 RSpec.describe ChromeTestingDownloader do
-  it "has a version number" do
+  it 'has a version number' do
     expect(ChromeTestingDownloader::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it '.latest_version' do
+    expect(ChromeTestingDownloader.latest_version).to be_a(String)
   end
 end
